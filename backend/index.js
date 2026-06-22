92d606e21862f92b83c705f8654822ba2434a7a5
Роман Бушуев <romanbushuev@MacBook-Air.local>
2026-06-22 21:52:47 +0800

    Remove stealth plugin due to HF block

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,6 @@
 import express from 'express';
 import { chromium } from 'playwright-extra';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import cors from 'cors';
-
-chromium.use(StealthPlugin());
 
 const app = express();
 
