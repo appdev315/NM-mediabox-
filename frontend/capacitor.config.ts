23800edbaee8ada0bbfab251a265f42a58e47b88
Your Name <you@example.com>
2026-07-09 15:39:43 +0800

    style: remove emojis from floating title, make it a 3D button, disable capacitor splash screen

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,13 +1,16 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.mediabox.app',
+  appId: 'com.romanbushuev.mediabox',
   appName: 'mediabox',
   webDir: 'dist',
   plugins: {
     CapacitorHttp: {
       enabled: true,
     },
+    SplashScreen: {
+      launchShowDuration: 0,
+    }
   },
   server: {
     cleartext: true,
