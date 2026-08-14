5e533876ca93b5927e07164c404bf9c64c300040
Your Name <you@example.com>
2026-08-14 08:13:56 +0800

    feat(proxy): add full 10-proxy Webshare pool with round-robin rotation

--- backend/go-scraper/streamer/liftw.go
+++ backend/go-scraper/streamer/liftw.go
@@ -57,6 +57,20 @@ func getHttpClient(timeout time.Duration) *http.Client {
 				}
 			}
 		}
+		if len(proxyUrls) == 0 {
+			proxyUrls = []string{
+				"http://ezyxagjl:vkx1ms0rbzk7@31.59.20.176:6754",
+				"http://ezyxagjl:vkx1ms0rbzk7@31.56.127.193:7684",
+				"http://ezyxagjl:vkx1ms0rbzk7@45.38.107.97:6014",
+				"http://ezyxagjl:vkx1ms0rbzk7@198.105.121.200:6462",
+				"http://ezyxagjl:vkx1ms0rbzk7@64.137.96.74:6641",
+				"http://ezyxagjl:vkx1ms0rbzk7@198.23.243.226:6361",
+				"http://ezyxagjl:vkx1ms0rbzk7@38.154.185.97:6370",
+				"http://ezyxagjl:vkx1ms0rbzk7@84.247.60.125:6095",
+				"http://ezyxagjl:vkx1ms0rbzk7@142.111.67.146:5611",
+				"http://ezyxagjl:vkx1ms0rbzk7@191.96.254.138:6185",
+			}
+		}
 	})
 
 	client := &http.Client{Timeout: timeout}
